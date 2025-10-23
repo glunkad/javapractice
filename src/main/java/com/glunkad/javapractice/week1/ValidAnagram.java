55071b99fce59301913a929a5429999326350686
Gunjan <glunkad26@gmail.com>
2025-10-23 20:56:54 +0530

    feat: added 2 questions

--- src/main/java/com/glunkad/javapractice/week1/ValidAnagram.java
+++ src/main/java/com/glunkad/javapractice/week1/ValidAnagram.java
@@ -1,5 +1,8 @@
 package com.glunkad.javapractice.week1;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 //Given two strings s and t, return true if t is an of s, and false otherwise.
 //Example 1:
 //Input: s = "anagram", t = "nagaram"
@@ -12,12 +15,42 @@ package com.glunkad.javapractice.week1;
 //s and t consist of lowercase English letters.
 //Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
 public class ValidAnagram {
-    public static boolean isAnagram(String s, String t) {
+
+    /*TC: O(nLog(n))*/
+    public static boolean isAnagram1(String s, String t){
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+        return Arrays.equals(sArray, tArray);
+    }
+
+//    TC : O(N) SC : O(1)
+    public static boolean isAnagram2(String s, String t){
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        int[] map = new int[26];
+
+        for(int i = 0; i < s.length(); i++){
+            map[s.charAt(i) - 'a']++;
+            map[t.charAt(i) - 'a']--;
+        }
+
+        for(int i = 0; i < 26; i++){
+            if(map[i] != 0){
+                return false;
+            }
+        }
+        return true;
 
     }
+
     public static void main(String[] args) {
         String s = "anagram";
         String t = "nagaram";
-        System.out.println(isAnagram(s, t));
+        System.out.println(isAnagram2(s, t));
     }
 }
