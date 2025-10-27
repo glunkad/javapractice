edee049c183d77c0e2d9dcd1f8a919232d20bfe1
gunjan <gunjan.lunkad@medimaze.in>
2025-10-27 09:52:49 +0530

    solved valid anagram

--- src/main/java/com/glunkad/javapractice/week1/ValidAnagram.java
+++ src/main/java/com/glunkad/javapractice/week1/ValidAnagram.java
@@ -1,5 +1,8 @@
 package com.glunkad.javapractice.week1;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 //Given two strings s and t, return true if t is an of s, and false otherwise.
 //Example 1:
 //Input: s = "anagram", t = "nagaram"
@@ -12,12 +15,60 @@ package com.glunkad.javapractice.week1;
 //s and t consist of lowercase English letters.
 //Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
 public class ValidAnagram {
-    public static boolean isAnagram(String s, String t) {
+    // Bruteforce O(NlogN+MlogM)
+    public static boolean isAnagram1(String s, String t) {
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
 
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+
+        for(int i  = 0; i < sArr.length; i++){
+            if(sArr[i] != tArr[i]){
+                return false;
+            }
+        }
+        return true;
     }
+
+
+    // TC : O(n+m) SC : O(1)
+    public static boolean isAnagram2(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        HashMap<Character, Integer> mapS = new HashMap<Character, Integer>();
+        HashMap<Character, Integer> mapT = new HashMap<Character, Integer>();
+        for(int i = 0 ; i < s.length(); i++){
+            mapS.put(s.charAt(i), mapS.getOrDefault(s.charAt(i), 0) + 1);
+            mapT.put(t.charAt(i), mapT.getOrDefault(t.charAt(i), 0) + 1);
+        }
+
+        return mapS.equals(mapT);
+    }
+
+    // TC : O(n+m) SC : O(1)
+    public static boolean isAnagram3(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int[] map = new int[26];
+        for(int i = 0; i < s.length(); i ++){
+            map[s.charAt(i) - 'a']++;
+            map[t.charAt(i) - 'a']--;
+        }
+
+        for(int i : map){
+            if( i != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         String s = "anagram";
         String t = "nagaram";
-        System.out.println(isAnagram(s, t));
+        System.out.println(isAnagram3(s, t));
     }
 }
